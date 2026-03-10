4946dd8d9f70b55ff9cb59808b8e16b7739cca51
Jiahui <jia.hui@hctm.ukm.edu.my>
2026-03-10 21:18:02 +0800

    Initial commit of HCTM calculators

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,35 @@
-/**
- * @license
- * SPDX-License-Identifier: Apache-2.0
- */
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home2";
-import BodyWeightCalculator from "./pages/BodyWeightCalculator";
-import PotassiumCalculator from "./pages/PotassiumCalculator";
-import SodiumCalculator from "./pages/SodiumCalculator";
-import MagnesiumCalculator from "./pages/MagnesiumCalculator";
-import TPNAssistant from "./pages/TPNAssistant";
-import NutritionGoals from "./pages/NutritionGoals";
+function App() {
+  const [count, setCount] = useState(0)
 
-export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/calculators/ibw-adjbw" element={<BodyWeightCalculator />} />
-        <Route path="/calculators/potassium" element={<PotassiumCalculator />} />
-        <Route path="/calculators/sodium" element={<SodiumCalculator />} />
-        <Route path="/calculators/magnesium" element={<MagnesiumCalculator />} />
-        <Route path="/calculators/tpn-assistant" element={<TPNAssistant />} />
-        <Route path="/calculators/nutrition-goals" element={<NutritionGoals />} />
-      </Routes>
-    </Router>
-  );
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
+
+export default App
